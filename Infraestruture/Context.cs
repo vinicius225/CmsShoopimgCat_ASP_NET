eb962e0192c320564645420e51a61468aa421acf
vinicius225 <vinicius.guilherme225@gmail.com>
2022-02-15 11:43:15 -0300

    Iniciando alteração no banco de dados

---
+++ Infraestruture/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoopimgCat_ASP_NET.Infraestruture
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
