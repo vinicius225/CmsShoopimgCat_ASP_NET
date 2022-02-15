651861ef19226002f768cb555be605f46ace473d
vinicius225 <vinicius.guilherme225@gmail.com>
2022-02-15 10:49:17 -0300

    primeiro git

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CmsShoopimgCat_ASP_NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
